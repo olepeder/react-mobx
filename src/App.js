5d1d9dcc0af06f8046d79c537ec8c0c0e323441c
Ole Peder Granaasen <ole.peder.granaasen@politiet.no>
2022-09-20 17:22:43 +0200

    CSS in JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-
+import styled from '@emotion/styled';
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr >
     <td>{pokemon.name.english}</td>
@@ -54,6 +54,33 @@ PokemonInfo.propTypes = {
   })
 };
 
+const Title = styled.h1`
+  text-align: center
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
+const Th = styled.th`
+  text-align: left;
+  font-size: x-large;
+`;
+
 function App() {
 
   const [filter, filterSet] = useState("");
@@ -67,29 +94,19 @@ function App() {
   }, []);
 
   return (
-    <div style={{
-      margin: 'auro',
-      width: 800,
-      paddingTop: '1rem'
-    }}>
-      <h1 className='title'>Pokemon Search</h1>
-      <input
+    <Container>
+      <Title>Pokemon Search</Title>
+      <Input
         value={filter}
         onChange={(e) => filterSet(e.target.value)}
       />
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem"
-        }}
-      >
+      <TwoColumnLayout>
         <div>
           <table width="100%">
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Type</th>
+                <Th>Name</Th>
+                <Th>Type</Th>
               </tr>
             </thead>
             <tbody>
@@ -109,9 +126,9 @@ function App() {
         {selectedItem && (
           <PokemonInfo {...selectedItem} />
         )}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
-export default App;
+export default App;;
